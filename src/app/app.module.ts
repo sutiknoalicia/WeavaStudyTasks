65906b5d9fa2fb520e4c553b336afe42be0d6566
Alicia Sutikno <sutiknoalicia@gmail.com>
2022-09-22 13:41:10 +0800

    Add observable and product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { ProductListComponent } from './products/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
-  declarations: [ AppComponent , ProductListComponent, ConvertToSpacesPipe, StarComponent ],
-  imports: [BrowserModule,FormsModule],
+  declarations: [ AppComponent , ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent ],
+  imports: [BrowserModule,FormsModule, HttpClientModule],
   bootstrap: [ AppComponent ],
 })
 export class AppModule { }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,44 +1,29 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, catchError, tap, throwError } from "rxjs";
 import { IProduct } from "./product";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
-export class ProductService{
-    getProducts(): IProduct[] {
-        return [
-          {
-            productId: 2,
-            productName: 'Garden Cart',
-            productCode: 'GDN-0023',
-            releaseDate: 'March 18, 2021',
-            description: '15 gallon capacity rolling garden cart',
-            price: 32.99,
-            starRating: 4.2,
-            imageUrl: 'assets/images/garden_cart.png',
-          },
-          {
-            productId: 5,
-            productName: 'Hammer',
-            productCode: 'TBX-0048',
-            releaseDate: 'May 21, 2021',
-            description: 'Curved claw steel hammer',
-            price: 8.9,
-            starRating: 4.8,
-            imageUrl: 'assets/images/hammer.png',
-          },
-          {
-            productId: 10,
-            productName: 'Video Game Controller',
-            productCode: 'GMG-0042',
-            releaseDate: 'October 15, 2020',
-            description: 'Standard two-button video game controller',
-            price: 35.95,
-            starRating: 4.6,
-            imageUrl: 'assets/images/xbox-controller.png',
-          }
-        ];
+export class ProductService {
+  private productUrl = 'api/products/products.json';
+  constructor(private http: HttpClient) {}
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All', JSON.stringify(data))),
+      catchError(this.handleError)
+    )
+  }
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
     }
-    
+    else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => errorMessage);
+  }
 }
